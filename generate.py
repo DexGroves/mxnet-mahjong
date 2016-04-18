c5a38c78f465a4fca38c55dfa42232f0a490a5ba
DexGroves <dex.groves@gmail.com>
2016-04-18 20:08:58 +0100

    Code to generate ponds and winners

--- generate.py
+++ generate.py
@@ -3,36 +3,63 @@ from code.hand import Hand
 from code.mahjong_player import MahjongPlayer
 
 
-tileset = ['1', '1', '1',
-           '2', '2', '2',
-           '3', '3', '3',
-           '4', '4', '4',
-           '5', '5', '5',
-           '6', '6', '6',
-           '7', '7', '7',
-           '8', '8', '8',
-           '9', '9', '9',
-           'D', 'D', 'D',
-           'E', 'E', 'E',
-           'S', 'S', 'S']
-
-
 def generate_wall(tileset):
     return random.sample(tileset, len(tileset))
 
 
+def generate_pond(player, max_len, tileset):
+    discard_pile = []
+    wall = generate_wall(tileset)
+    hand = Hand(wall[0:5])
+    wall = wall[6:len(wall)]
+
+    # 10 discards max
+    while len(wall) > (len(tileset) - 4 - max_len):
+        if hand.is_complete():
+            return discard_pile, hand[len(hand) - 1]
+        else:
+            discard, hand = player.make_discard(hand)
+            discard_pile.append(discard)
+            hand.append(wall.pop(0))
+
+    return discard_pile, None
+
+
+def tiles_to_numeric_vector(tiles, max_len, tileset):
+    """
+    Convert a set of tiles to a list of numbers
+    corresponding to their index in unique_tiles.
+    """
+    tile_indices = [tileset.index(x) for x in tiles]
+
+    if len(tile_indices) > max_len:
+        raise ValueError("Too many tiles!")
+    else:
+        remaining_len = max_len - len(tile_indices)
+        tile_indices += ([-1] * remaining_len)
+
+    return tile_indices
+
+
+# Hyperparameters
+n_trials = 1000
+max_len = 10
+
+
+unique_tiles = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'D', 'E', 'S']
+tileset = unique_tiles + unique_tiles + unique_tiles
+
 player = MahjongPlayer()
 
-discard_pile = []
-wall = generate_wall(tileset)
-hand = Hand(wall[0:5])
-wall = wall[6:len(wall)]
+# Spam n_trials ponds and write to csv
+f = open('data/ponds.csv', 'w')
+for i in xrange(n_trials):
+    pond, winning_tile = generate_pond(player, max_len, tileset)
 
-if hand.is_complete():
-    print 'Tsumo!'
-    print ', '.join(hand)
-else:
-    discard, hand = player.make_discard(hand)
-    discard_pile.append(discard)
-    hand.append(wall.pop(0))
-    print ', '.join(discard_pile)
+    if winning_tile is not None:
+        pond_ind = tiles_to_numeric_vector(pond, max_len, unique_tiles)
+        out_str = ','.join([str(winning_tile),
+                           ','.join([str(x) for x in pond_ind])])
+
+        f.write(out_str + '\n')
+f.close()
