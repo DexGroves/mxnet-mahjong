ce0e740ad0e85040fbf6f6bd1a6d0ce63cb1b3d7
DexGroves <dex.groves@gmail.com>
2016-04-19 22:33:40 +0100

    Update MahjongPlayer

--- code/mahjong_player.py
+++ code/mahjong_player.py
@@ -1,39 +1,15 @@
-import copy
 import collections
+from code.tile_evaluator import TileEvaluator
 
 
 class MahjongPlayer(object):
     """Handle logical deductions on hands for simple 12-tile mahjong."""
 
     def __init__(self):
-        pass
+        self.evaluator = TileEvaluator()
 
     def make_discard(self, hand):
-        c_hand = copy.copy(hand)
-
-        unpaired = self.get_unpaired(c_hand)
-
-        # One unpaired tile? Discard it! Shanpon waits are good (!)
-        if len(unpaired) == 1:
-            c_hand.remove(unpaired[0])
-            return unpaired[0], c_hand
-
-        # Particularly useless unpaired tiles? Discard!
-        if 'wS' in unpaired:
-            c_hand.remove('wS')
-            return 'wS', c_hand
-        if 'wE' in unpaired:
-            c_hand.remove('wE')
-            return 'wE', c_hand
-        if 'dW' in unpaired:
-            c_hand.remove('dW')
-            return 'dW', c_hand
-
-        # Else discard the lowest number. A laughable strategy.
-        min_unpaired = sorted(unpaired)[0]
-        c_hand.remove(str(min_unpaired))
-
-        return str(min_unpaired), c_hand
+        return self.evaluator.evaluate_optimal_cut(hand)
 
     @staticmethod
     def get_unpaired(hand):
---
+++ code/tests/test_mahjong_player.py
@@ -0,0 +1,10 @@
+from code.tile import Tile
+from code.hand import Hand
+from code.mahjong_player import MahjongPlayer
+
+
+def test_make_discard():
+    mp = MahjongPlayer()
+    test_hand = Hand([Tile(x) for x in ['s1', 's1', 's1', 's2', 's3', 's4',
+                                        's5', 's7', 'dW', 'wE']])
+    assert mp.make_discard(test_hand) in test_hand
